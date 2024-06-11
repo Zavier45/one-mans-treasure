0eaa831ff8900ac9685f6c6b3cdb992218f9920f
Zavier Hopson <zo.hopson94@gmail.com>
2024-06-11 15:53:52 -0500

    created

---
+++ Models/UserProfile.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace OneMansTreasure.Models;
+
+public class UserProfile
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    [NotMapped]
+    public string UserName { get; set; }
+    [NotMapped]
+    public string Email { get; set; }
+    public string IdentityUserId { get; set; }
+    public IdentityUser IdentityUser { get; set; }
+    public List<Sale> Sales { get; set; }
+    public string FullName
+    {
+        get
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
