6d6fbbfb3caab9da7fb8877d7523624901f6d55e
Zavier Hopson <zo.hopson94@gmail.com>
2024-06-13 10:17:50 -0500

    sales path added

--- client/src/components/ApplicationViews.jsx
+++ client/src/components/ApplicationViews.jsx
@@ -6,16 +6,25 @@ import Register from "./auth/Register";
 export default function ApplicationViews({ loggedInUser, setLoggedInUser }) {
   return (
     <Routes>
-      {/* insert specific routes here */}
-      <Route
-        path="login"
-        element={<Login setLoggedInUser={setLoggedInUser} />}
-      />
-      <Route
-        path="register"
-        element={<Register setLoggedInUser={setLoggedInUser} />}
-      />
-      <Route path="*" element={<p>Nothing to see here, folks.</p>} />
+      <Route path="/">
+        <Route
+          index
+          element={
+            <AuthorizedRoute loggedInUser={loggedInUser}>
+              <>Home Page</>
+            </AuthorizedRoute>
+          }
+        />
+        <Route
+          path="login"
+          element={<Login setLoggedInUser={setLoggedInUser} />}
+        />
+        <Route
+          path="register"
+          element={<Register setLoggedInUser={setLoggedInUser} />}
+        />
+        <Route path="*" element={<p>Nothing to see here, folks.</p>} />
+      </Route>
     </Routes>
   );
 }
