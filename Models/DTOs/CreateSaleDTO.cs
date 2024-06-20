a7e061357ea1a1ba99e3c86ba92c4caeaf80867b
Zavier Hopson <zo.hopson94@gmail.com>
2024-06-20 12:17:45 -0500

    new property added

--- Models/DTOs/CreateSaleDTO.cs
+++ Models/DTOs/CreateSaleDTO.cs
@@ -6,6 +6,8 @@ public class CreateSaleDTO
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
     public string Address { get; set; }
+    public string FeaturedItem { get; set; }
+    public string FeaturedItemDesc { get; set; }
     public int SaleHostId { get; set; }
     public int[] SaleTypes { get; set; }
 
