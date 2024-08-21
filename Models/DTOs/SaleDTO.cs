32dd4043daeb8fce91c9e46bc3666b2d2b11ffc4
Zavier Hopson <zo.hopson94@gmail.com>
2024-08-21 14:41:47 -0500

    update properties of SaleDTO

--- Models/DTOs/SaleDTO.cs
+++ Models/DTOs/SaleDTO.cs
@@ -3,9 +3,15 @@ namespace OneMansTreasure.Models.DTOs;
 public class SaleDTO
 {
     public int Id { get; set; }
+    public string Title { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    public string Address { get; set; }
+    public int NeighborhoodId { get; set; }
+    public NeighborhoodDTO Neighborhood { get; set; }
+    public string StreetAddress { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
     public string FeaturedItem { get; set; }
     public string FeaturedItemDesc { get; set; }
     public int SaleHostId { get; set; }
