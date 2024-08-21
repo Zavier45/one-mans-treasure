ed90741c44cafcd991850d951695f1620c1ce8cd
Zavier Hopson <zo.hopson94@gmail.com>
2024-08-21 14:25:54 -0500

    updated name schemes

--- Models/DTOs/CreateSaleDTO.cs
+++ Models/DTOs/CreateSaleDTO.cs
@@ -2,10 +2,14 @@ namespace OneMansTreasure.Models.DTOs;
 
 public class CreateSaleDTO
 {
-
+    public string Title { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    public string Address { get; set; }
+    public int NeighborhoodId { get; set; }
+    public string StreetAddress { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
     public string FeaturedItem { get; set; }
     public string FeaturedItemDesc { get; set; }
     public int SaleHostId { get; set; }
