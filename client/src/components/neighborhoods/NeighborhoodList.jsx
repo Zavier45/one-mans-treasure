aa8dcc3423ae44f30137caadae1faa21a982489b
Zavier Hopson <zo.hopson94@gmail.com>
2024-08-21 14:38:54 -0500

    created neighborhoods

---
+++ client/src/components/neighborhoods/NeighborhoodList.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export const NeighborhoodList = ({ loggedInUser }) => {
+  const [neighborhood, setNeighborhood] = React.useState("");
+};
